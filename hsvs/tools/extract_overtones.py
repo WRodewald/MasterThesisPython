334495a885ddc5a7509bd515e3ec472019598a12
WRodewald <w.rodewald@web.de>
2020-06-25 13:31:42 +0200

    Rerun export of original / harmonic with properly truncated time. Added multicore support to harmonic extraction

--- hsvs/tools/extract_overtones.py
+++ hsvs/tools/extract_overtones.py
@@ -2,6 +2,7 @@ import numpy as np
 from .framed_audio import FramedAudio
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import multiprocessing
 
 # this function extracts some overtones
 def extract_ovetones(frame, pitch, pitch_inc, fs, num_overtones = 10, resynthesize = True):
@@ -43,7 +44,10 @@ def synthesize_overtones(overtones, pitch, pitch_inc, fs, N):
     resynth = np.real(np.sum(resynth,1)) / 2
     return resynth
 
-def extract_overtones_from_audio(audio, pitch = None, pitch_inc = None, num_overtones = 40):
+def extract_ovetones_wrap(args):
+    return extract_ovetones(*args)
+
+def extract_overtones_from_audio(audio, pitch = None, pitch_inc = None, num_overtones = 40, num_threads=1):
     
     if(pitch == None): 
         pitch = audio.get_trajectory('pitch')
@@ -54,11 +58,31 @@ def extract_overtones_from_audio(audio, pitch = None, pitch_inc = None, num_over
     window = np.hanning(audio.block_size)
     overtones = np.zeros([audio.get_num_frames(), num_overtones], dtype='complex128')
 
-    print('Extracting overtones ...')
-    for i in tqdm(range(audio.get_num_frames())):
-        frame = audio.get_frame(i) * window
+    N = audio.get_num_frames()
 
-        overtones[i,:]  = extract_ovetones(frame, pitch[i], pitch_inc[i], audio.fs, num_overtones, False)
+    print('Extracting overtones ...')
+    if(num_threads == 1):        
+        # analysis per frame
+        for i in tqdm(range(audio.get_num_frames())):
+            frame = audio.get_frame(i) * window
+            overtones[i,:]  = extract_ovetones(frame, pitch[i], pitch_inc[i], audio.fs, num_overtones, False)
+
+    else:
+        # multi threaded
+
+        # collect inputs
+        inputs = []
+        for i in range(N):
+            frame = audio.get_frame(i) * window
+            inputs.append((frame,  pitch[i], pitch_inc[i], audio.fs, num_overtones, False))
+        
+        # schedule workers and run
+        with multiprocessing.Pool(processes=num_threads) as pool:            
+            results = list(tqdm(pool.imap(extract_ovetones_wrap, inputs, chunksize=1), total=N ))
+
+        # collect outputs
+        for i in range(N):
+            overtones[i,:] = results[i][:]
 
     return overtones
 
